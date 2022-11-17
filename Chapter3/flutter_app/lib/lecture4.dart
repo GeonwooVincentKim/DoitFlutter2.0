ea51c0dd47117de119208a576066bc422511c1ed
dukim <sajacall82@naver.com>
2022-11-17 20:51:58 +0900

    ch03 ch04 update

---
+++ Chapter3/flutter_app/lib/lecture4.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  final String title = 'BuildContext Sample';
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: title,
+      home: const MyStatelessWidget(),
+    );
+  }
+}
+
+/*
+1. title을 받아와서 Appbar title에 표시해보겠습니다.
+2. BottomSheet가 정상적으로 표시되도록 BuildContext를 이용해 오류를 해결해보겠습니다.
+*/
+
+class MyStatelessWidget extends StatelessWidget {
+  const MyStatelessWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Demo')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('SHOW BOTTOM SHEET'),
+          onPressed: () {
+            // ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            //   content: (
+            //     const Text('Close BottomSheet')
+            //   ),
+            //   action: SnackBarAction(
+            //     label: 'ACTION',
+            //     onPressed: () { },
+            //   ),
+            // ));
+            Scaffold.of(context).showBottomSheet(
+                  (BuildContext context) {
+                return Container(
+                  alignment: Alignment.center,
+                  height: 200,
+                  color: Colors.amber,
+                  child: Center(
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        const Text('BottomSheet'),
+                        ElevatedButton(
+                          child: const Text('Close BottomSheet'),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
